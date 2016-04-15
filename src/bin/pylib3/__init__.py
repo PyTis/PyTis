d03c093c9cd9ecdb6c6b1d823a3f68506a4b92f3
PyTis <lee_joshua_r@ne.bah.com>
2016-04-15 02:05:05 +0000

    removing awslib from __init__

--- src/bin/pylib3/__init__.py
+++ src/bin/pylib3/__init__.py
@@ -1,7 +1,6 @@
-from . import awslib
 from . import configobj
 from . import _version
 from . import validate
 from .csv import parse
 from .csv import CSVParser
-__all__ = ['awslib', 'configobj', 'CSVParser', 'parse', '_version', 'validate']
+__all__ = ['configobj', 'CSVParser', 'parse', '_version', 'validate']
