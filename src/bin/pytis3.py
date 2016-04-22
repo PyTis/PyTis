0cf31d88fbed398f9c6e7141fb31a59c60f000f7
PyTis <lee_joshua_r@ne.bah.com>
2016-04-22 20:35:30 +0000

    added new Exception type

--- src/bin/pytis3.py
+++ src/bin/pytis3.py
@@ -72,6 +72,7 @@ __input_options__ = ['y','N']
 # #############################################################################
 class QuitNow(Exception): pass
 class FileNotFound(UserWarning): pass
+class FileExists(UserWarning): pass
 class EmptyTemplate(FileNotFound): pass
 class DuplicateCopyright(UserWarning): pass
 class IdiotError(Exception): pass
