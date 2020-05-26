09c3c31338b3aedaae3575bdf3f39aa37a49fb62
PyTis <GitHub@PyTis.com>
2020-05-26 02:46:11 -0400

    moved, built

---
+++ src/build/lib/pytis/__init__.py
@@ -0,0 +1,13 @@
+import sys
+
+python_version = float("%s.%s"%(sys.version_info.major,sys.version_info.minor))
+
+
+if python_version >= 3.0:
+	from . import pytis3 as PyTis
+else:
+  import pytis as PyTis
+
+__all__ = ['python_version', 'PyTis']
+
+
