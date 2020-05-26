6dab4bcf47c6391a18901b9f94ead3b7585bb870
PyTis <GitHub@PyTis.com>
2020-05-26 00:15:54 -0400

    MOVED FROM 'bin' to 'pytis' DIRECTORY

--- src/bin/pylib3/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import configobj
-from . import _version
-from . import validate
-from .csv import parse
-from .csv import CSVParser
-__all__ = ['configobj', 'CSVParser', 'parse', '_version', 'validate']
