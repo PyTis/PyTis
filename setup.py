904084d7ae176c53d1d04ff26e47d3dd477fe4aa
PyTis <GitHub@PyTis.com>
2020-05-26 05:12:05 -0400

    .

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
-
-"""setup
-=====
-"""
-#from distutils.core import setup
-from setuptools import setup, find_packages
-import imp
-import optparse
-import os
+import codecs
+import os.path
+import re
 import sys
 
 __curdir__ = os.path.abspath(os.path.dirname(__file__))
@@ -16,35 +10,157 @@ __version__ = '1.0'
 __author__ = 'Josh Lee'
 __copyright__ = 'PyTis, LLC.'
 
+
+python_version = float("%s.%s"%(sys.version_info.major,sys.version_info.minor))
+
+def import_by_path(fpath, module_name):
+	#die(os.path.abspath(os.path.dirname((fpath))))
+	global __curdir__
+	fpath = os.path.abspath(os.path.join(__curdir__,fpath))
+	fdir = os.path.dirname(fpath)
+	os.chdir(os.path.abspath((fdir)))
+
+	if python_version >= 3.5:
+		import importlib.util
+		spec = importlib.util.spec_from_file_location(module_name, fpath)
+		mod = importlib.util.module_from_spec(spec)
+		spec.loader.exec_module(mod)
+		os.chdir(__curdir__)
+		return mod
+	elif python_version >= 3.3:
+		from importlib.machinery import SourceFileLoader
+		mod = SourceFileLoader(module_name, fpath).load_module()
+		os.chdir(__curdir__)
+		return mod
+	else:
+		import imp
+		mod = imp.load_source(module_name, fpath)
+		os.chdir(__curdir__)
+		return mod
+
+
+from setuptools import setup, find_packages
+
+def die(string=None):
+	global log
+	try:
+		if log and string:
+			log.fatal(string)
+
+	except NameError:
+		if string:
+			print("output: '%s'" % string)
+	else:
+		if not log and string:
+			print("output: '%s'" % string)
+
+	sys.exit()
+DIE=Die=die
+
+
+
+
+
+def read(*parts):
+	fpath = os.path.abspath(os.path.join(__curdir__, *parts))
+	handle = codecs.open(fpath, 'r')
+	ret_val = handle.read()
+	handle.close()
+	del handle
+	return ret_val
+	
+
+
+def load_pytis():
+	pytis_path = 'src/pytis/pytis.py'
+	sys.path.append(os.path.abspath(os.path.join(__curdir__,'src/')))
+	sys.path.append(os.path.abspath(os.path.join(__curdir__,'src/pytis/')))
+	sys.path.append(os.path.abspath(os.path.join(__curdir__,'src/pytis/pylib')))
+	sys.path.append(os.path.abspath(os.path.join(__curdir__,'src/pytis/pylib3')))
+	PyTis=import_by_path(pytis_path, 'pytis')
+	return PyTis
+
+
+	#raise RuntimeError("Unable to find version string.")
+
+
+install_requires = [
+#    'botocore==1.16.12',
+    'docutils>=0.10,<0.16',
+#    'rsa>=3.1.2,<=3.5.0',
+#    's3transfer>=0.3.0,<0.4.0',
+]
+
 file_handle = open('README.md','r')
 readme_contents = file_handle.read(-1)
 file_handle.close()
 
-file_handle = open('LICENSE','r')
-license_contents = file_handle.read(-1)
-file_handle.close()
+#file_handle = open('LICENSE.txt','r')
+#license_contents = file_handle.read(-1)
+#file_handle.close()
 
-#os.chdir(os.path.abspath(os.path.join(os.curdir,'src')))
+'''
+if sys.version_info[:2] == (3, 4):
+    install_requires.append('PyYAML>=3.10,<5.3')
+    install_requires.append('colorama>=0.2.5,<0.4.2')
+else:
+    install_requires.append('PyYAML>=3.10,<5.4')
+    install_requires.append('colorama>=0.2.5,<0.4.4')
+'''
 
-def run(opts,args):
-	"""setup run doc help"""
-	setup_options=dict(
-				name='PyTis-pytisconsulting',
-				version=PyTis.__version__,
-				author='Josh Lee',
-				author_email='pytis@PyTis.com',
-				description='pytis-toolkit',
-				license=license_contents,
-				long_description=readme_contents,
-				long_description_content_type="text/markdown",
-				url='https://github.com/PyTis/PyTis',
-				classifiers=[
-					"Development Status :: %s - Production/Stable" % PyTis.__version__,
+PyTis = load_pytis()
+
+os.chdir(os.path.abspath(os.path.join(__curdir__,'src')))
+setup_options = dict(
+    name='pytis',
+    version=PyTis.__version__,
+
+    author=__author__,
+		author_email='pytis@PyTis.com',
+
+		description='pytis-toolkit',
+		url='https://github.com/PyTis/PyTis',
+
+		long_description=readme_contents,
+		long_description_content_type="text/markdown",
+
+    packages=find_packages( include=[
+										'pytis*',
+			],
+		exclude=['src/.zombie_hunter_files',
+																		'src/etc*',
+																		'src/log*']),
+    package_data={
+			'pytis' : [ 'pytis*' ],
+		},
+		data_files = [
+			('', [os.path.abspath(os.path.join(__curdir__,'LICENSE.txt')),
+						os.path.abspath(os.path.join(__curdir__,'README.md')),
+						os.path.abspath(os.path.join(__curdir__,'PyTis_Logo.jpg'))
+					 ]
+			),
+		],
+
+		#license_file=os.path.abspath(os.path.join(__curdir__,'LICENSE')),
+
+    scripts=[
+			os.path.abspath(os.path.join(__curdir__, 'src/bin/copyright')),
+			os.path.abspath(os.path.join(__curdir__, 'src/pytis/copyright.py')),
+			os.path.abspath(os.path.join(__curdir__, 'src/bin/bulkcopy')),
+			os.path.abspath(os.path.join(__curdir__, 'src/bin/bulkmove'))
+		],
+    install_requires=install_requires,
+    extras_require={},
+    license="PyTis LICENSE 3.0",
+		#license=license_contents,
+    classifiers=[
+					'Development Status :: 5 - Production/Stable',
 					'Intended Audience :: Developers',
 					'Intended Audience :: System Administrators',
 					'Natural Language :: English',
 					'Programming Language :: Python :: 3',
-					'License :: OSI Approved :: PyTis License',
+#					'License :: OSI Approved :: PyTis License',
+					'License :: Other/Proprietary License',
 					'Operating System :: OS Independent',
 					'Programming Language :: Python',
 					'Programming Language :: Python :: 2',
@@ -55,157 +171,25 @@ def run(opts,args):
 					'Programming Language :: Python :: 3.6',
 					'Programming Language :: Python :: 3.7',
 					'Programming Language :: Python :: 3.8',
-
-				],
-				packages=[
-										'src.pytis',
-#										'src/pytis/pytis.py',
-#										'src/pytis/configure.py',
-#										'src/pytis/*.py',
-										'src.pytis.pylib',
-										'src.pytis.pylib.util',
-										'src.pytis.pylib3',
-										'src.pytis.pylib3.util',
-				],
-				scripts=[
-#					os.path.abspath(os.path.join(os.curdir,'src/bin/copyright')),
-					'src/bin/copyright.py',
-					'src/bin/copyright'
-				],
-			)
-	setup(**setup_options)
-
-def main():
-	"""usage: setup"""
-	#filename = os.path.abspath(os.path.join(PyTis.__configdir__, '%s.ini' %
-	#os.path.basename(os.path.abspath(sys.argv[0])))) #main.__doc__ = "%s\n\n
-	#CONFIG FILE: %s" % (main.__doc__,os.path.abspath(filename))
+    ],
+)
 
 
-	errors=[]
-	help_dict = dict(version=__version__,
-						 author=__author__,
-						 created=__created__,
-						 copyright=__copyright__)
-	parser = optparse.OptionParser()
+if 'py2exe' in sys.argv:
+    # This will actually give us a py2exe command.
+    import py2exe
+    # And we have some py2exe specific options.
+    setup_options['options'] = {
+        'py2exe': {
+            'optimize': 0,
+            'skip_archive': True,
+            'dll_excludes': ['crypt32.dll'],
+            'packages': ['docutils', 'urllib', 'httplib', 'HTMLParser',
+                         'awscli', 'ConfigParser', 'xml.etree', 'pipes'],
+        }
+    }
+    setup_options['console'] = ['bin/aws']
 
-	parser.extra_txt = "\n\n%s\n" % run.__doc__ + """
 
-examples:	
-	xxx
-
-SEE ALSO:
-	xxx
-
-COPYRIGHT:
-	%(copyright)s
-
-AUTHOR:
-	%(author)s
-
-HISTORY:
-	Original Author
-
-VERSION:
-	%(version)s
-""" % help_dict
-
-	parser.formatter.format_description = lambda s:s
-	parser.set_description(__doc__)
-	parser.set_usage(main.__doc__)
-
-	runtime = optparse.OptionGroup(parser, "-- RUNTIME ARGUMENTS")
-	parser.add_option_group(runtime)
-	# -------------------------------------------------------------------------
-	# variable setting
-	vars = optparse.OptionGroup(parser, "-- CONFIGURATION SETTINGS")
-	parser.add_option_group(vars)
-	# ----------------------------
-	dbgroup = optparse.OptionGroup(parser, "-- DEBUG")
-	dbgroup.add_option("-D", "--debug", action="store_true",
-					 default=False, dest='debug',
-					 help="Enable debugging")
-	helpishere=False
-	for a in sys.argv:
-		if a == '--help':
-			helpishere=True
-			dbgroup.add_option("-V", "--verbose", action="store_true",
-							 default=True, dest='verbose',
-							 help="Be more Verbose (make lots of noise)")
-	if not helpishere:
-		dbgroup.add_option("-V", "--verbose", action="store_true",
-						 default=True, dest='verbose',
-						 help=optparse.SUPPRESS_HELP)
-
-	dbgroup.add_option("-q", "--quiet", action="store_true",
-					 default=False, dest='quiet',
-					 help="be vewwy quiet (I'm hunting wabbits)")
-
-	dbgroup.add_option("-v", "--version", action="store_true",
-					 default=False, dest='version',
-					 help="Display Version")
-
-	parser.add_option_group(dbgroup)
-	# ----------------------------
-
-	(opts, args) = parser.parse_args()
-	if opts.quiet: opts.verbose = False
-
-	#if opts.action is None and len(args) and args[0] in
-	#	('start','stop','restart','status'):
-
-	#	opts.action = args[0]
-	#	del args[0]
-
-	old_version = opts.version
-	opts.version = True
-	#log = PyTis.set_logging(opts, os.path.basename(sys.argv[0]))
-	
-	opts.version = old_version
-
-	'''
-	if opts.version:
-		return PyTis.version(__version__)
-	'''
-
-	#if opts.action and len(args) == 1 and args[0] in \
-	#	'start stop restart status'.split() and opts.action != args[0]:
-	#	errors.append("Silly human, you provided an action via a flag (%s) and \
-	# an option on STDIN (%s) and they are different.	Please only provide one \
-	# action." % (opts.action, args[0]))
-
-	#if len(args) == 1 and args[0] in 'start stop restart status'.split() and
-	#	opts.action is None:
-
-	#	opts.action = args[0]
-	#	del args[0]
-
-	if len(args) == 0 and not errors:
-		return parser.print_usage()
-	elif not errors:
-		try:
-			run(opts, args)
-		except KeyboardInterrupt as e:
-			#log.debug("Keyboard-Interrupt, bye!")
-			print("Keyboard-Interrupt, bye!")
-			if not opts.quiet:
-				# log.info("\nbye!")
-				print("\nbye!")
-			return
-		else:
-			# log.info("Done.")
-			print("Done.")
-			return
-	else:
-		parser.print_usage()
-		if errors:
-			# log.error(str("\n".join(errors)))
-			print(str("\n".join(errors)))
-		return parser.print_help(errors)
-
-	parser.print_help("ERROR: Unknown, but invalid input.")
-	sys.exit(0)
-
-if __name__ == '__main__':
-		main()
+setup(**setup_options)
 
