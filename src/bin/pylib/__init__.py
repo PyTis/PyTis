dc900df0854590f6bc01706868f5c88514aa7763
PyTis <GitHub@PyTis.com>
2020-05-26 00:15:12 -0400

    MOVED FROM 'bin' to 'pytis' DIRECTORY

--- src/bin/pylib/__init__.py
+++
@@ -1,5 +0,0 @@
-import configobj
-from . import validate
-from csv import parse
-from csv import CSVParser
-__all__ = ['configobj', 'CSVParser', 'parse', 'validate']
--- src/bin/pylib/util/num.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['is_positive_int']
-def is_positive_int(value):
-    try:
-        value = int(value)
-    except ValueError:
-        return False
-    return value > 1
-
