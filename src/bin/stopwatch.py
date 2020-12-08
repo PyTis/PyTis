327ae0eff7a2f82f05c3a146d095e1d03599915f
PyTis <GitHub@PyTis.com>
2020-12-08 01:43:45 -0500

    updated to also include singleton

--- src/bin/stopwatch.py
+++ src/bin/stopwatch.py
@@ -7,7 +7,7 @@
 #                                                                              #
 #     http://www.PyTis.com/License/                                            #
 #                                                                              #
-#     Copyright (c) 2009 Josh Lee                                              #
+#     Copyright (c) 2014 Josh Lee                                              #
 #                                                                              #
 # Software distributed under the License is distributed on an "AS IS" basis,   #
 # WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License     #
@@ -19,10 +19,11 @@
 """ This is a simple class that is used to time how long commands take to run.
 
 EXAMPLE CODE:
+
+  #############################################################################
   # Passing True into the constructor, tells the stopwatch to reset it's timer
   # each and every time it is used/called.
   s=StopWatch(True)
-
 
   print("DynamoDB cleanup is now running %s " % \\
     datetime.datetime.now().strftime("%I:%M%%s %m %b, %Y") % \\
@@ -57,17 +58,54 @@ EXAMPLE OUTPUT:
   ...........................................................
   table test_users has been deleted - ( it took 60.9334 seconds)
 
+  #############################################################################
+  # Passing True into the second argument of the constructor, tells the primary
+  # stopwatch to have a child stopwatch, this is used to track total time.
+
+  EXAMPLE OF REMEDIATION BOOTSTRAP:
+  from csisutil.awslambda import AwsLambda, logCall, stopwatch
+
+  stopwatch=StopWatch(True, True)
+  stopwatch.setDefaultPrefixes()
+  
+  Output from a child instance of AWSLambda has the following output (small
+  snippet).
+
+EXAMPLE OUTPUT:
+ --> ./bootstrap.py -v -d
+AwsLambda.__init__() called || Time elapsed since last call 209.5699 Âµ microseconds || Total Time 209.3315 Âµ microseconds
+GlueBootstrap bootstraper is now running for account: 084135731370 at 10:56 PM on Nov 3, 2020
+AwsLambda.run() called || Time elapsed since last call 0.3243 seconds || Total Time 0.3245 seconds
+GlueBootstrap.Run() called || Time elapsed since last call 44.5843 Âµ microseconds || Total Time 0.3246 seconds
+GlueBootstrap.Clean() called || Time elapsed since last call 25.7492 Âµ microseconds || Total Time 0.3246 seconds
+GlueBootstrap.cleanGlue() called || Time elapsed since last call 20.9808 Âµ microseconds || Total Time 0.3246 seconds
+GlueBootstrap.cleanDatabase() called || Time elapsed since last call 28.1334 Âµ microseconds || Total Time 0.3246 seconds
+GlueBootstrap.cleanCrawler() called || Time elapsed since last call 18.3582 Âµ microseconds || Total Time 0.3247 seconds
+GlueBootstrap.cleanJob() called || Time elapsed since last call 17.4046 Âµ microseconds || Total Time 0.3247 seconds
+GlueBootstrap.cleanConnection() called || Time elapsed since last call 16.6893 Âµ microseconds || Total Time 0.3247 seconds
+AwsLambda.closingDown() called || Time elapsed since last call 0.3242 seconds || Total Time 0.5621 seconds
+AwsLambda.printExit() called || Time elapsed since last call 48.399 Âµ microseconds || Total Time 0.5621 seconds
+
+  # As seen above, by passing a second "True" into the constructor, we get a
+  # second stopwatch, allowing us to also track the total time.
+
+
 """
 import time
 
-__all__ = ('StopWatch','Stopwatch')
+try:
+  from pytis import pytis as PyTis
+except:
+  import pytis.pytis as PyTis
+
+__all__ = ('StopWatch','Stopwatch', 'stopwatch')
 
 # copied from my  (PyTis) library, slightly modified for Verizon Wireless.
 __copyright__ = 'PyTis.com'
 __author__ = 'Josh Lee'
 __created__ = '06:14pm 09 Sep, 2014'
-__modified__ '09:11am 02 Feb, 2020'
-__version__ = 1.1
+__modified__ = '01:45am 08 Dec, 2020'
+__version__ = 1.3
 
 
 # Copied from my personal coding library.
@@ -86,9 +124,16 @@ class StopWatch():
   reset_each_call=False
 
   # decimals is used when formatting the output to be more "human-readable"
-  decimals=4
+  _decimals=4
 
-  def __init__(self,reset_each_call=False):
+  _advanced_mode = False
+  StopWatch = None
+  parent = False
+
+  # displayed before time is output
+  _prefix = '|| '
+
+  def __init__(self, reset_each_call=False, advanced_mode=False):
     """
 
     The intention is for this class's instance to be treated as almost a
@@ -123,24 +168,60 @@ class StopWatch():
     """
     self.t=time.time()
     self.reset_each_call=reset_each_call
+    self.setAdvancedMode(advanced_mode)
+
+  @property
+  def hasParent(self):
+    return self.parent
+
+  def setAdvancedMode(self,bol):
+    """
+    Show two timers
+    """
+    self._advanced_mode = bol
+    if bool(self._advanced_mode) and not self.hasParent:
+      self.StopWatch = StopWatch(False)
+      self.StopWatch.parent=True
+      self.reset_each_call = True
+
+  def getAdvancedMode(self):
+    return self._advanced_mode
 
   def __call__(self):
     """
-    this could be a shortcut to stop or reset this timer
+    Returns the time passed thusfar.
     """
-    pass
+    return self.read()
 
   def __str__(self):
     """
     Print the time as a string (human readable / formatted)
     """
-    return self.beautify(self.read())
+    return self.beautify( self.read() )
 
   def __repr__(self):
     """
     Print the time as a string (unformatted)
     """
-    return str(self.read())
+    return str( self.read() )
+
+  def __ne__(self, other):
+    return float( self.read() ) != float(other)
+
+  def __eq__(self, other):
+    return float( self.read() ) == float(other)
+
+  def __le__(self, other):
+    return float( self.read() ) <= float(other)
+
+  def __lt__(self, other):
+    return float( self.read() ) < float(other)
+
+  def __ge__(self, other):
+    return float( self.read() ) >= float(other)
+
+  def __gt__(self, other):
+    return float( self.read() ) > float(other)
 
   def read(self):
     """
@@ -175,6 +256,19 @@ class StopWatch():
     self._stopped=True
 
   def beautify(self, t):
+    if self.getAdvancedMode():
+      return self.fullBeauty(t, self.StopWatch.read())
+    else:
+      return self.singleBeauty(t)
+    
+  def fullBeauty(self, t, tt):
+    """
+      Display both clocks/timers.
+      Time since last call || Total time since object was instantiated.
+    """
+    return "%s%s" % ( self.singleBeauty(t), self.StopWatch.singleBeauty(tt) )
+
+  def singleBeauty(self, t):
     """
     Args:
       t (float) : float representation of time.time instnace value
@@ -191,20 +285,82 @@ class StopWatch():
       > 20.0882 seconds
     """
     t=float(t)
+    # IF t is greater than 1 tenth of a second.
+    if t > 0.1: 
+      return "%s%s seconds" % (self.prefix, round(t, self.decimals))
 
-    if t > 0.1: return "%s seconds" % round(t, self.decimals)
-
-    if t >= 0.001:
+    # IF t is greater than 1 thousandth of a second.
+    elif t >= 0.001:
+      # IF t is exactly 1 thousandth of a second, no plural label
       if float(t) == float(0.001): label = ''
+      # else set label to a plural s
       else: label = 's'
-      return "%s milisecond%s OR %s seconds" % (round(1000*t, self.decimals+4),
-        label, round(t, self.decimals))
+      
+      return "%s%s milisecond%s OR %s seconds" % (self.prefix,
+        round(1000*t, self.decimals),
+        label, str(round(t, self.decimals)) )
 
-    if t >= 0.000001:
+    # IF t is greater than 1 milionth of a second.
+    elif t >= 0.000001:
       if float(t) == float(0.000001): label = " Âµs microsecd"
       else: label = 'Âµ microseconds'
-      return "%s %s" % (round(t,4), label)
+      return "%s%s %s" % (self.prefix, round(1000000*t, self.decimals), label)
+    else:
+      return "%s%s seconds" % (self.prefix, round(t, self.decimals))
+
+  def clearPrefixes(self):
+    self.prefix = '|| '
+    if self.StopWatch:
+      self.StopWatch.prefix = '|| '
+
+  def setDefaultPrefixes(self):
+    self.prefix = '|| Time elapsed since last call ' # 'Elapsed'
+    if self.StopWatch:
+      self.StopWatch.prefix = ' || Total Time '
+
+  def set_prefix(self, prefix):
+    self._prefix = prefix
+  def get_prefix(self):
+    return self._prefix
+  prefix=property(get_prefix, set_prefix)
+
+  def setDefaultDecimals(self):
+    self.setDecimals(4)
+    
+  def set_decimals(self, decimals):
+    self._decimals = decimals
+  def get_decimals(self):
+    """
+    The unique IF statement, where decimals is set to None, is because 
+    round( float(0.222) , 0 ) returns 0.2 instead of 0
+    Apparently, even though logic would dictate that 0 would mean Zero digits, 
+    the only way to display NO digits after the decimal is to set decimals to
+    None.
+    """
+    if not self._decimals and self._decimals == 0: self.decimals = None
+    return self._decimals
+  decimals=property(get_decimals, set_decimals)
+
+  def setChildDecimals(self, decimals):
+    if self.StopWatch:
+      self.StopWatch.decimals = decimals
+    else:
+      raise PyTis.ProgrammerError('There is not a child StopWatch object ' \
+        'to set decimals on.')
+
+  def setDecimals(self, decimals):
+    """ 
+    This can be called to set the decimal places to be used in all cases
+    (parent and child) or, you can utilize them individually.
+    """
+    self.decimals=decimals
+    if self.StopWatch:
+      self.StopWatch.decimals=decimals
+
 Stopwatch=StopWatch
+# singleton
+stopwatch=StopWatch(True, True)
+stopwatch.setDefaultPrefixes()
 
 if __name__ == '__main__':
   import pydoc, sys
