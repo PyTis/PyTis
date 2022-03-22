9f2fc0da334e2ee55d52963001603890ecb3924b
PyTis <github@pytis.com>
2022-03-22 10:50:54 -0400

    .

--- src/bootstrap-box/setup-user.py
+++ src/bootstrap-box/setup-user.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python3
 
-import os
-if not os.path.exists('/home/leejo7a/bashrc.orig'):
-	print('making backup of .bashrc file')
-	os.system('cp /home/leejo7a/.bashrc /home/leejo7a/bashrc.orig')
+import getpass
+import os, sys
 
-print('copying all files from bootstrap/home-jlee to /home/leejo7a')
+username = 'jlee'
+original_username = username
+
+print('INIT...')
+
+if len(sys.argv) > 1:
+	u = sys.argv[1]
+	if str(u).lower() in \
+		('-h','/h','h','/?','-?','?','-help','/help','help'):
+		print('USAGE: ./setup-user.py [optional: <username>]')
+		print('this script will by default, install for the "jlee" user, unless ' \
+			'you provide a different username (you must be logged in as that ' \
+			'user, to override the username).')
+
+	else:
+		pu = str(u)
+		if pu.lower() == str(getpass.getuser()).lower():
+			username = pu
+		else:
+			print('ERROR: username "%s" does not match logged in user "%s"' % (
+				pu, getpass.getuser()))
+			print('You must be logged in as "%s" to install for "%s."' % (pu, pu))
+			sys.exit(0)
+
+
+print('installing for %s' % username)
+
+if not os.path.exists('/home/%s/bashrc.orig' % username):
+	print('making backup of .bashrc file')
+	os.system('cp /home/%s/.bashrc /home/%s/bashrc.orig' % (username, username))
+
+print('copying all files from bootstrap/home-jlee to /home/%s' % username)
 os.system('/bin/cp -rf home-jlee/* ../')
 os.system('/bin/cp -rf home-jlee/.sessions/ ../')
 os.system('/bin/cp -rf home-jlee/.ssh ../')
@@ -27,7 +56,9 @@ os.system('/bin/cp -rf home-jlee/.pypirc ../')
 os.system('/bin/cp -rf home-jlee/.screenrc ../')
 os.system('/bin/cp -rf home-jlee/.vimrc ../')
 
-#os.system('/bin/cp -rf home-jlee/.bashrc ../')
 
+if username != original_username:
+	os.system("find .  -type f -name '*' |xargs sed  -i 's/%s/%s/g'" % (
+		original_username, username))
 
-
+print("\ndone.")
